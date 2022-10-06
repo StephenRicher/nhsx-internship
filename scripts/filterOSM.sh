d188df847295c26086cdf9196f3518eb98a8e860
Stephen Richer <sr467@bath.ac.uk>
2022-10-06 11:00:46 +0100

    Update map.

--- scripts/filterOSM.sh
+++ scripts/filterOSM.sh
@@ -1,10 +1,10 @@
 # Download from: https://download.geofabrik.de/europe/great-britain/england.html
 
-wget-P ../data/ https://download.geofabrik.de/europe/great-britain/england-latest.osm.pbf
+wget -P ../data/ https://download.geofabrik.de/europe/great-britain/england-latest.osm.pbf
 
 conda activate ox
 # Extract region by bbox
-osmium extract --bbox 0.477,51.77,1.628,52.61 ../data/england-latest.osm.pbf -o ../data/esneft.osm
+osmium extract --bbox 0.38,51.74,1.68,52.42 ../data/england-latest.osm.pbf -o ../data/esneft.osm
 # Filter for roads only
 osmium tags-filter -o ../data/esneft-highways.osm ../data/esneft.osm nw/highway
 
